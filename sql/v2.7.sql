9dc5e76988e9f05cf6a321b503c807341e3ff26a
ICTKaBoE <ict.kaboe@coltd.be>
2023-05-17 11:12:32 +0200

    sql update v2.7

--- sql/v2.7.sql
+++ sql/v2.7.sql
@@ -1,2 +1,24 @@
-UPDATE tbl_setting SET moduleId=1, settingTabId=1, name='Versie', `type`='input', `options`=NULL, value=0x322E37, `order`=2, deleted=0 WHERE id='site.version';
-INSERT INTO tbl_module_setting (moduleId, `key`, value, deleted) VALUES(3, 'informNewToIds', NULL, 0);
+CREATE TABLE `tbl_management_beamer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `schoolId` int(11) NOT NULL DEFAULT 0,
+  `buildingId` int(11) NOT NULL DEFAULT 0,
+  `roomId` int(11) NOT NULL DEFAULT 0,
+  `brand` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `serialnumber` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `tbl_management_printer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `schoolId` int(11) NOT NULL DEFAULT 0,
+  `buildingId` int(11) NOT NULL DEFAULT 0,
+  `roomId` int(11) NOT NULL DEFAULT 0,
+  `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `brand` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `serialnumber` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `colormode` set('B','YMCB') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'B',
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
