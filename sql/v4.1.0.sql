b8913c4b15c60d20a623dc12ace7b595387359a3
ICTKaBoE <ict.kaboe@coltd.be>
2025-03-17 07:51:26 +0100

    bugfix update db

--- sql/v4.1.0.sql
+++ sql/v4.1.0.sql
@@ -1,5 +1,5 @@
---CREATE
-CREATE TABLE `tbl_management_cctv` (
+--CREATE;
+CREATE TABLE IF NOT EXISTS `tbl_management_cctv` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `guid` varchar(36) NOT NULL,
   `schoolId` int(11) NOT NULL,
@@ -14,23 +14,23 @@ CREATE TABLE `tbl_management_cctv` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---RENAME
+--RENAME;
 RENAME TABLE tbl_order_purchase TO tbl_order;
 RENAME TABLE tbl_order_purchase_line TO tbl_order_line;
 
---ALTER
+--ALTER;
 ALTER TABLE tbl_order_line CHANGE purchaseId orderId int(11) NOT NULL;
 ALTER TABLE tbl_order ADD quoteLink TEXT NULL;
 ALTER TABLE tbl_order CHANGE quoteLink quoteLink TEXT NULL AFTER supplierId;
 ALTER TABLE tbl_order ADD quoteFile varchar(40) NULL;
 ALTER TABLE tbl_order CHANGE quoteFile quoteFile varchar(40) NULL AFTER quoteLink;
 
---INSERT
+--INSERT;
 INSERT INTO tbl_navigation (routeGroupId, parentId, `order`, redirect, link, name, icon, color, minimumRights, settings, deleted) VALUES('1', 26, 13, 0, 'cctv', 'CCTV-Camera', 'device-cctv', 'blue', '0000001', NULL, 0);
 INSERT INTO tbl_setting (id, settingTabId, name, `type`, `options`, value, readonly, `order`, deleted) VALUES('db.version', 1, 'DB Versie', 'input', NULL, 0x342E312E30, 1, 99, 0);
 
---UPDATE
+--UPDATE;
 UPDATE tbl_navigation SET `order`=14 WHERE id=39;
 UPDATE tbl_navigation SET link='order' WHERE id=45;
 
---DELETE
+--DELETE;
--- update_db.php
+++ update_db.php
@@ -2,7 +2,6 @@
 
 use Database\Database;
 use Ouzo\Utilities\Arrays;
-use Ouzo\Utilities\Strings;
 use Router\Helpers;
 use Security\Code;
 
@@ -14,11 +13,12 @@ ob_implicit_flush();
 
 $version = Helpers::url()->getParam("v");
 $file = file_get_contents("./sql/v{$version}.sql");
+$file = preg_replace('/^--.+$/m', "", $file);
 $lines = explode(";", $file);
+$lines = Arrays::map($lines, fn($l) => trim($l));
 $lines = Arrays::filterNotBlank($lines);
-$lines = Arrays::filter($lines, fn($l) => !Strings::startsWith($l, "--"));
-$lines = Arrays::map($lines, fn($l) => trim($l) . ";");
-$lines = array_values($lines);
+$lines = Arrays::map($lines, fn($l) => "{$l};");
+die(var_dump($lines));
 
 $db = Database::getInstance();
 $connection = $db->getConnection();
