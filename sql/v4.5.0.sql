b69d6bdb861d1f688416657412580477a788f6be
ICTKaBoE <ict.kaboe@coltd.be>
2025-09-23 13:31:23 +0200

    db sql

--- sql/v4.5.0.sql
+++ sql/v4.5.0.sql
@@ -147,200 +147,6 @@ CREATE TABLE `tbl_order_status` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `tbl_registration` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `status` varchar(100) NOT NULL,
-  `registrationByUserId` int(11) NOT NULL,
-  `name` varchar(254) NOT NULL,
-  `firstName` varchar(254) NOT NULL,
-  `callName` varchar(254) DEFAULT NULL,
-  `sex` set('x','m','v') NOT NULL DEFAULT 'x',
-  `birthDate` date NOT NULL,
-  `birthPlace` varchar(254) NOT NULL,
-  `birthCountryId` int(11) NOT NULL,
-  `hasInsz` tinyint(1) NOT NULL DEFAULT 0,
-  `insz` varchar(16) NOT NULL,
-  `nationalityId` int(11) DEFAULT NULL,
-  `phone` varchar(32) DEFAULT NULL,
-  `email` varchar(254) DEFAULT NULL,
-  `addressStreet` varchar(254) NOT NULL,
-  `addressNumber` int(11) NOT NULL,
-  `addressBus` varchar(8) DEFAULT NULL,
-  `addressZipcode` varchar(8) NOT NULL,
-  `addressCity` varchar(254) NOT NULL,
-  `addressCountryId` int(11) NOT NULL,
-  `subscriberRelation` set('P','F','M','G','GF','GM','FF','FM','S','B','PF','PM','CF','CM','I','O') NOT NULL DEFAULT 'O',
-  `doctorName` varchar(254) DEFAULT NULL,
-  `doctorNumber` varchar(32) DEFAULT NULL,
-  `meansOfTransport` set('F','B','MB','C','LB','T') NOT NULL DEFAULT 'F',
-  `personalReligion` set('52', '63', '135', '136', '140', '187', '194', '225', '418', '9999') NOT NULL DEFAULT '9999',
-  `livingWith` set('P','PD','F','M','I','G','GF','GM','S','B','O') NOT NULL DEFAULT 'O',
-  `livingWithOther` varchar(254) DEFAULT NULL,
-  `diedParent` varchar(64) DEFAULT NULL,
-  `livingWithIName` varchar(254) NOT NULL,
-  `bankaccountName` varchar(254) DEFAULT NULL,
-  `bankaccountIban` varchar(32) DEFAULT NULL,
-  `schoolyearId` int(11) NOT NULL,
-  `schoolId` int(11) NOT NULL,
-  `studyyearId` int(11) NOT NULL,
-  `fieldId` int(11) NOT NULL,
-  `optionId` int(11) DEFAULT NULL,
-  `talentId` int(11) DEFAULT NULL,
-  `clilId` int(11) DEFAULT NULL,
-  `sitWith` varchar(254) DEFAULT NULL,
-  `sitNotWith` varchar(254) DEFAULT NULL,
-  `lastSchool` set('N','B','S','H') DEFAULT 'N',
-  `startAt` DATE NOT NULL,
-  `mealMonday` set('L','H') DEFAULT 'L',
-  `mealTuesday` set('L','H') DEFAULT 'L',
-  `mealThursday` set('L','H') DEFAULT 'L',
-  `mealFriday` set('L','H') DEFAULT 'L',
-  `lastSchoolStudyyearB` set('1','2','3','4','5','6') DEFAULT NULL,
-  `lastSchoolNameB` varchar(254) DEFAULT NULL,
-  `lastSchoolCountryB` int(11) DEFAULT NULL,
-  `lastSchoolZipcodeB` varchar(8) DEFAULT NULL,
-  `lastSchoolCityB` varchar(254) DEFAULT NULL,
-  `lastSchoolHasCertificateB` tinyint(1) NOT NULL DEFAULT 0,
-  `lastSchoolAdviceB` varchar(254) DEFAULT NULL,
-  `lastSchoolCertificateReceivedB` set('Y','L') DEFAULT NULL,
-  `lastSchoolBaSoCertificateReceivedB` set('N','Y','L') DEFAULT NULL,
-  `lastStudyyearS` set('1','2','3','4','5','6','7') DEFAULT NULL,
-  `lastSchoolFieldS` varchar(254) DEFAULT NULL,
-  `lastSchoolNameS` varchar(254) DEFAULT NULL,
-  `lastSchoolCountryS` int(11) DEFAULT NULL,
-  `lastSchoolZipcodeS` varchar(8) DEFAULT NULL,
-  `lastSchoolCityS` varchar(254) DEFAULT NULL,
-  `lastSchoolCertificateS` set('A','B','C') DEFAULT NULL,
-  `lastSchoolClauseS` varchar(254) DEFAULT NULL,
-  `lastStudyyearH` set('1','2','3','4','5') DEFAULT NULL,
-  `lastSchoolFieldH` varchar(254) DEFAULT NULL,
-  `lastSchoolNameH` varchar(254) DEFAULT NULL,
-  `lastSchoolCountryH` int(11) DEFAULT NULL,
-  `lastSchoolZipcodeH` varchar(8) DEFAULT NULL,
-  `lastSchoolCityH` varchar(254) DEFAULT NULL,
-  `picture` tinyint(1) DEFAULT NULL,
-  `classPicture` tinyint(1) DEFAULT NULL,
-  `passInfo` tinyint(1) DEFAULT NULL,
-  `measurement` tinyint(1) DEFAULT NULL,
-  `smartschool` tinyint(1) DEFAULT NULL,
-  `website` tinyint(1) DEFAULT NULL,
-  `socials` tinyint(1) DEFAULT NULL,
-  `newsletter` tinyint(1) DEFAULT NULL,
-  `homeLanguage` int(11) DEFAULT NULL,
-  `homeLanguageMore` varchar(254) DEFAULT NULL,
-  `howLongDutch` set('1','1-5','5') DEFAULT NULL,
-  `problemLearn` tinyint(1) DEFAULT NULL,
-  `problemFamily` tinyint(1) DEFAULT NULL,
-  `problemHealth` tinyint(1) DEFAULT NULL,
-  `problemLearnProblem` set('ADD','ADHD','ASS','DCD','DYSC','DYSL','DYSO','NLD','O') DEFAULT NULL,
-  `problemLearnProblemOther` text DEFAULT NULL,
-  `problemLearnCertificate` tinyint(1) DEFAULT NULL,
-  `problemLearnCertificateReceived` set('H','R','CLB') DEFAULT NULL,
-  `problemLearnGuidance` text DEFAULT NULL,
-  `problemLearnExtra` text DEFAULT NULL,
-  `problemFamilyFamily` text DEFAULT NULL,
-  `problemFamilyPersonal` text DEFAULT NULL,
-  `problemHealthNotify` text DEFAULT NULL,
-  `problemHealthDoDont` text DEFAULT NULL,
-  `problemHealthMedication` tinyint(1) DEFAULT NULL,
-  `problemHealthMedicationWhat` text DEFAULT NULL,
-  `problemHealthConsult` tinyint(1) DEFAULT NULL,
-  `problemHealthInternalConsult` set('N','T','B','D') DEFAULT NULL,
-  `conditions` tinyint(1) DEFAULT NULL,
-  `remarks` text DEFAULT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `tbl_registration_contact` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `registrationId` int(11) NOT NULL,
-  `followNumber` int(11) NOT NULL DEFAULT 1,
-  `name` varchar(254) NOT NULL,
-  `firstName` varchar(254) NOT NULL,
-  `relation` set('F','M','G','GF','GM','FF','S','B','PF','PM','CF','CM','I','O') NOT NULL DEFAULT 'O',
-  `relationOther` varchar(254) DEFAULT NULL,
-  `phonePrivate` varchar(32) DEFAULT NULL,
-  `phoneWork` varchar(32) DEFAULT NULL,
-  `phoneMobile` varchar(32) DEFAULT NULL,
-  `email` varchar(254) DEFAULT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `tbl_registration_field` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `schoolId` int(11) NOT NULL,
-  `schoolyearId` int(11) NOT NULL,
-  `studyyearId` int(11) NOT NULL,
-  `name` varchar(254) NOT NULL,
-  `administrativeGroupNumber` varchar(8) NOT NULL,
-  `maxCapacity` int(11) NOT NULL DEFAULT 20,
-  `capacityType` SET('C', 'F') NOT NULL DEFAULT 'C',
-  `full` tinyint(1) NOT NULL DEFAULT 0,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `tbl_registration_field_clil` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `schoolId` int(11) NOT NULL,
-  `schoolyearId` int(11) NOT NULL,
-  `studyyearId` int(11) NOT NULL,
-  `fieldId` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `tbl_registration_field_option` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `schoolId` int(11) NOT NULL,
-  `schoolyearId` int(11) NOT NULL,
-  `studyyearId` int(11) NOT NULL,
-  `fieldId` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `tbl_registration_field_talent` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `schoolId` int(11) NOT NULL,
-  `schoolyearId` int(11) NOT NULL,
-  `studyyearId` int(11) NOT NULL,
-  `fieldId` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `tbl_registration_schoolyear` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `visibleFrom` date NOT NULL,
-  `visibleUntil` date DEFAULT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `tbl_registration_studyyear` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(36) NOT NULL,
-  `schoolId` int(11) NOT NULL,
-  `schoolyearId` int(11) NOT NULL,
-  `name` varchar(254) NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 CREATE TABLE `tbl_source` (
   `id` varchar(64) NOT NULL,
   `tokenType` varchar(64) DEFAULT NULL,
@@ -385,8 +191,8 @@ UPDATE tbl_route SET routeGroupId=2, `method`='ANY', route='{view}/general/{what
 
 INSERT INTO tbl_source (id, identityEndpoint, identityGrantType, identityClientId, identityClientSecret, identityScope, deleted) VALUES('kaboe', 0x68747470733A2F2F7777772E6964656E746974797365727665722E62652F636F6E6E6563742F746F6B656E, 'client_credentials', 'informat_customer_KaBoe', 'mxl0onpr4KPlAOI2fn6ma4gy_9qfis0BLXt5NHB6lx4', 0x6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E766F6F72696E73636872696A76696E67656E2E3032343830320D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E766F6F72696E73636872696A76696E67656E2E3032343831310D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E766F6F72696E73636872696A76696E67656E2E3032343833360D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E766F6F72696E73636872696A76696E67656E2E3131303036340D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E766F6F72696E73636872696A76696E67656E2E3131353931350D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E766F6F72696E73636872696A76696E67656E2E3131353932330D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E6C6565726C696E67656E2E3032343830320D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E6C6565726C696E67656E2E3032343831310D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E6C6565726C696E67656E2E3032343833360D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E6C6565726C696E67656E2E3131303036340D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E6C6565726C696E67656E2E3131353931350D0A6170695F696E666F726D61745F7361735F6C6565726C696E67656E2E6C6565726C696E67656E2E3131353932330D0A6170695F696E666F726D61745F7361735F706572736F6E65656C2E706572736F6E65656C2E3032343830320D0A6170695F696E666F726D61745F7361735F706572736F6E65656C2E706572736F6E65656C2E3032343831310D0A6170695F696E666F726D61745F7361735F706572736F6E65656C2E706572736F6E65656C2E3032343833360D0A6170695F696E666F726D61745F7361735F706572736F6E65656C2E706572736F6E65656C2E3131303036340D0A6170695F696E666F726D61745F7361735F706572736F6E65656C2E706572736F6E65656C2E3131353931350D0A6170695F696E666F726D61745F7361735F706572736F6E65656C2E706572736F6E65656C2E313135393233, 0);
 
-ALTER TABLE db_intranet_v4.tbl_helpdesk ADD subject varchar(254) NULL;
-ALTER TABLE db_intranet_v4.tbl_helpdesk CHANGE subject subject varchar(254) NULL AFTER category;
+ALTER TABLE tbl_helpdesk ADD subject varchar(254) NULL;
+ALTER TABLE tbl_helpdesk CHANGE subject subject varchar(254) NULL AFTER category;
 
 ALTER TABLE tbl_navigation DROP COLUMN minimumRights;
 
@@ -403,7 +209,7 @@ ALTER TABLE tbl_school CHANGE sourceId sourceId VARCHAR(8) DEFAULT '' NOT NULL A
 
 ALTER TABLE tbl_security_group DROP COLUMN permission;
 
-ALTER TABLE tbl_country CHANGE translatedName `name` VARCHAR(254) NOT NULL AFTER officialName;
+ALTER TABLE tbl_general_country CHANGE translatedName `name` VARCHAR(254) NOT NULL AFTER officialName;
 
 ALTER TABLE tbl_informat_classgroup ADD administrativeGroupCode varchar(8) NOT NULL;
 ALTER TABLE tbl_informat_classgroup CHANGE administrativeGroupCode administrativeGroupCode varchar(8) NOT NULL AFTER schoolyear;
